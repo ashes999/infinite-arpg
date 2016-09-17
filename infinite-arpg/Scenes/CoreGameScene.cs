0dcc1ca06037cb5a65a0fe17bfaab241758daee9
ashes999 <ashes999@yahoo.com>
2016-09-17 01:19:07 -0400

    JS integration via Jurassic.NET ClearScript requires on V8 which requires kernel32.dll, no go on Mono

--- infinite-arpg/Scenes/CoreGameScene.cs
+++ infinite-arpg/Scenes/CoreGameScene.cs
@@ -11,6 +11,10 @@ namespace infinitearpg
         public CoreGameScene(GraphicsDevice graphicsDevice) : base(graphicsDevice)
         {
             this.player = this.LoadImage("Content/player.png");
+
+            var engine = new Jurassic.ScriptEngine();
+            engine.SetGlobalValue("console", new Jurassic.Library.FirebugConsole(engine));
+            engine.Execute("console.log('hi!');");
         }
 
         public override void Draw(SpriteBatch spriteBatch)
