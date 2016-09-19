425198714aa3b66e7a991171b11a6cd5c84ff3bf
ashes999 <ashes999@yahoo.com>
2016-09-19 18:12:56 -0400

    Convert Drawable into C# ECS class

--- infinite-arpg/Content/Scripts/CoreGameScene.py
+++ infinite-arpg/Content/Scripts/CoreGameScene.py
@@ -1,37 +1,14 @@
-﻿from Microsoft.Xna.Framework import Color, Rectangle, Vector2
-from Microsoft.Xna.Framework.Graphics import SurfaceFormat, Texture2D
+﻿from Microsoft.Xna.Framework import Color, Vector2
 
+from DeenGames.InfiniteArpg.Ecs.Components import Drawable
 from DeenGames.InfiniteArpg.Scenes import AbstractScene
-
-class Drawable():
-    def __init__(self):
-        self.isColour = None
-
-    def image(self, imageTexture):
-        self.isColour = False
-        self.texture2D = imageTexture
-        return self
-
-    def colour(self, colourTexture, width, height):
-        self.isColour = True
-        self.texture2D = colourTexture
-        self.width = width
-        self.height = height
-        return self
-
-    def draw(self, spriteBatch):
-        if (self.isColour == True):
-            spriteBatch.Draw(self.texture2D, None, Rectangle(0, 0, self.width, self.height))
-        else:
-            spriteBatch.Draw(self.texture2D, Vector2.Zero)
-
 
 class CoreGameScene(AbstractScene):
     def __init__(self, graphicsDevice):
         super(CoreGameScene, self).__init__()
         self.ClearColour = Color.DarkGreen
-        self.player = Drawable().colour(self.Colour(Color.Red), 32, 32)
+        self.player = Drawable().Colour(self.Colour(Color.Red), 32, 32)
 
     def Draw(self, spriteBatch):
-        self.player.draw(spriteBatch)
+        self.player.Draw(spriteBatch)
 
---
+++ infinite-arpg/Ecs/Components/Drawable.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace DeenGames.InfiniteArpg.Ecs.Components
+{
+    public class Drawable
+    {
+        private bool isColour = false;
+        private Texture2D texture2D;
+        private int width = 0;
+        private int height = 0;
+
+        public Drawable Image(Texture2D imageTexture)
+        {
+            this.isColour = false;
+            this.texture2D = imageTexture;
+            return this;
+        }
+
+
+        public Drawable Colour(Texture2D colourTexture, int width, int height)
+        {
+            this.isColour = true;
+            this.texture2D = colourTexture;
+            this.width = width;
+            this.height = height;
+            return this;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            if (this.isColour == true)
+            {
+                spriteBatch.Draw(this.texture2D, null, new Rectangle(0, 0, this.width, this.height));
+            }
+            else
+            {
+                spriteBatch.Draw(this.texture2D, Vector2.Zero);
+
+            }
+        }
+    }
+}
+
